7d6237fa5bc3f61d4794f0164125283e0e5e095a
Vamsi Krishna Gattupalli <quic_vgattupa@quicinc.com>
2022-03-24 17:44:13 +0530

    msm: adsprpc: Wait for actual shutdown to complete
    Wait for shutdown to complete, before further unmapping
    memory in release.
    
    Change-Id: Ic574a3e851f99aa946b2e062fa7a752c64b9f2b2
    Acked-by: Deepika Singh <dsi@qti.qualcomm.com>
    Signed-off-by: Vamsi Krishna Gattupalli <quic_vgattupa@quicinc.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -651,6 +651,7 @@ struct fastrpc_file {
 	bool is_ramdump_pend;
 	/* Flag to indicate dynamic process creation status*/
 	enum fastrpc_process_create_state dsp_process_state;
+	struct completion shutdown;
 };
 
 static struct fastrpc_apps gfa;
@@ -4374,7 +4375,7 @@ static int fastrpc_release_current_dsp_process(struct fastrpc_file *fl)
 	}
 	VERIFY(err, fl->apps->channel[cid].issubsystemup == 1);
 	if (err) {
-		err = -ECONNRESET;
+		wait_for_completion(&fl->shutdown);
 		goto bail;
 	}
 	tgid = fl->tgid;
@@ -5821,6 +5822,7 @@ static int fastrpc_device_open(struct inode *inode, struct file *filp)
 	spin_lock(&me->hlock);
 	hlist_add_head(&fl->hn, &me->drivers);
 	spin_unlock(&me->hlock);
+	init_completion(&fl->shutdown);
 	return 0;
 }
 
@@ -6422,6 +6424,8 @@ static int fastrpc_restart_notifier_cb(struct notifier_block *nb,
 {
 	struct fastrpc_apps *me = &gfa;
 	struct fastrpc_channel_ctx *ctx;
+	struct fastrpc_file *fl;
+	struct hlist_node *n;
 	struct notif_data *notifdata = (struct notif_data *)data;
 	int cid = -1;
 
@@ -6434,6 +6438,16 @@ static int fastrpc_restart_notifier_cb(struct notifier_block *nb,
 		ctx->ssrcount++;
 		ctx->issubsystemup = 0;
 		mutex_unlock(&me->channel[cid].smd_mutex);
+	} else if (code == SUBSYS_AFTER_SHUTDOWN) {
+		pr_info("adsprpc: %s: %s subsystem is down\n",
+			__func__, gcinfo[cid].subsys);
+		spin_lock(&me->hlock);
+		hlist_for_each_entry_safe(fl, n, &me->drivers, hn) {
+			if (fl->cid != cid)
+				continue;
+			complete(&fl->shutdown);
+		}
+		spin_unlock(&me->hlock);
 	} else if (code == SUBSYS_RAMDUMP_NOTIFICATION) {
 		if (cid == RH_CID) {
 			if (me->ramdump_handle)
